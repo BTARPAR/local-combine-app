8ce75cf814f804ec982eb3efe2aceb13365f6edf
Bhargav <tarpara_bhargav@yahoo.in>
2021-06-16 18:49:40 -0700

    DONE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import BarChart from './components/BarChart'
+import Sphere from './components/Sphere'
+import TreeView from './components/Tree-View'
+import data from './constant'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/demo-1">Bar Chart</Link>
+            </li>
+            <li>
+              <Link to="/demo-2">Demo - 2</Link>
+            </li>
+            <li>
+              <Link to="/demo-3">Redux Tree</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/demo-1">
+            <BarChart/>
+          </Route>
+          <Route path="/demo-2">
+            <Sphere/>
+          </Route>
+          <Route path="/demo-3">
+            <TreeView data={data} width={600} height={500} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Sphere/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Canvas } from "@react-vertex/core";
+import { useViewportSize } from "@react-vertex/dom-hooks";
+import Scene from "./components/Scene";
+
+function Sphere() {
+  const { width, height } = useViewportSize();
+
+  return (
+    <Canvas width={width} height={height}>
+      <Scene />
+    </Canvas>
+  );
+}
+
+
+export default Sphere
